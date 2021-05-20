bf14ea13bc979e745ab42d3c2af487b0bf8f2975
Abhishek Bharadwaz <55627658+4Marsha1@users.noreply.github.com>
2021-05-20 19:28:07 +0530

    Update RepoComponent.js

--- src/components/RepoComponent.js
+++ src/components/RepoComponent.js
@@ -7,7 +7,7 @@ const RepoComponent = (props) => {
             <div className='details'>
                 <h1>{props.repo.name}</h1>
                 <h3>Language: {props.repo.language}</h3>
-                <h3>{props.repo.html_url}</h3>
+                <a href={props.repo.html_url} target='_blank'><h3>{props.repo.html_url}</h3></a>
                 <div className='description'>
                     <p>{props.repo.description}</p>
                 </div>
